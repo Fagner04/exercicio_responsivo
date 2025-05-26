9de5826c44af246341d319d6c0db5817d2326741
Fagner04 <Fagnerc04@gmail.com>
2025-05-25 23:42:33 -0300

    novo

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.fa-bars').on('click', function(){
+        $('#mobile_menu').slideToggle();
+        // $('#mobile_btn').find('i').toggleClass('fa-x');
+        
+
+    })
+})
